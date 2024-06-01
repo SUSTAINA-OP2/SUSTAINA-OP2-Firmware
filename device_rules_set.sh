bbb28146f14bd9d9f54206cc8b6fb06b62085b04
hiroki-0001 <touhu.273@gmail.com>
2024-06-01 20:51:39 +0900

    feat(device rules): デバイスルールを自動で適用させるためのスクリプトを追加

---
+++ device_rules_set.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -euxo pipefail
+
+HOME_DIR="${HOME}"
+device_rules_path=/etc/udev/rules.d/
+
+cd "$(dirname "$0")"
+
+if [ -e /etc/udev/rules.d/17_DK-42688-P.rules ]; then
+	sudo rm /etc/udev/rules.d/17_DK-42688-P.rules
+fi
+sudo cp  ./DK-42688-P/17_DK-42688-P.rules $device_rules_path
+
+if [ -e /etc/udev/rules.d/13_FTDI_Serial.rules ]; then
+	sudo rm /etc/udev/rules.d/13_FTDI_Serial.rules
+fi
+sudo cp ./USB-to-Quad-RS-485-Conv-Module/13_FTDI_Serial.rules $device_rules_path
+
+if [ -e /etc/udev/rules.d/18_FT4232H.rules ]; then
+	sudo rm /etc/udev/rules.d/18_FT4232H.rules
+fi
+sudo cp ./USB-to-Quad-RS-485-Conv-Module/18_FT4232H.rules $device_rules_path
+
+sudo udevadm control --reload-rules && udevadm trigger
+
+cat << EOS
+=========================================
+device rules setup success!!!
+=========================================
+EOS
